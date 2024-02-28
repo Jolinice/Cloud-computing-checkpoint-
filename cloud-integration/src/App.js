a5821a26a9adeb84d43f8279a2cd4e9d13674f60
SeyiBot <pricetwice007gmail.com>
2024-02-28 16:42:37 +0100

    Cloud integration checkpoint

---
+++ cloud-integration/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PortfolioLandingPage from "./components/home/home";
+import MapContainer from './components/map/mapContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <PortfolioLandingPage/>
+      <MapContainer/>
+    </div>
+  );
+}
+
+export default App;
